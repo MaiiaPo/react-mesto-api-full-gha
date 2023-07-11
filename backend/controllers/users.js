00de2eb09712fe0013dc77a11b49c016919e9d9f
Maiia <maiiapo.com@gmail.com>
2023-07-11 10:41:01 +0500

    Поправлены ошибки линтера

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -110,7 +110,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
-        { expiresIn: '7d' }
+        { expiresIn: '7d' },
       );
       return res.send({ token });
     })
